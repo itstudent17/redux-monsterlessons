f42304f2c10bd70e21aa94633c262260d7be3943
itstudent17 <itstudent17@outlook.com>
2021-02-15 19:57:55 +0300

    1-lesson

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log("Hello");
+import { createStore } from "redux";
+
+// reducer
+function playlist(state = [], action) {
+  console.log(action);
+  switch (action.type) {
+    case "ADD_TRACK":
+      return [...state, action.payload];
+  }
+
+  return state;
+}
+
+// createStore from Redux
+const store = createStore(playlist);
+
+// console.log(store.getState());
+store.subscribe(() => console.log("subscribe", store.getState()));
+
+store.dispatch({ type: "ADD_TRACK", payload: "Smells like teen spirit" });
+store.dispatch({ type: "ADD_TRACK", payload: "Enter Sandman" });
